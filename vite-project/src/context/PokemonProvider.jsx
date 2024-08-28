cdf0a8ce02511d0e4ef26fb7653f0a046e97190b
Taifinkaba <taifinkabad@gmail.com>
2024-08-28 19:06:57 -0400

    PokemonContext.jsx, PokemonProvider.jsx, main.jsx

--- vite-project/src/context/PokemonProvider.jsx
+++ vite-project/src/context/PokemonProvider.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import handleFetch from '../utils/handleFetch';
 
 // TODO: Import the PokemonContext
+import PokemonContext from "./PokemonContext";
 
 const starterPokemon = [
     {
@@ -31,13 +32,28 @@ const PokemonProvider = ({ children }) => {
     const [allPokemon, setAllPokemon] = useState(starterPokemon);
 
     // TODO: use useEffect to fetch data from the local JSON server (remember to start JSON server!)
-
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            const [data, error] = await handleFetch('http://localhost:4000/pokemon');
+            if (error) {
+                console.error(error);
+                return;
+            }
+            setAllPokemon(data);
+        };
+        // console
+        console.log(allPokemon);
+        
+        fetchPokemon();
+    }, []);
     // TODO: Add values to be included in the context here
-    let contextValues = {}
+    let contextValues = { allPokemon, setAllPokemon }
 
     // TODO: Wrap the {children} in the PokemonContext.Provider and provide the values above
     return (
-        { children }
+        <PokemonContext.Provider value={contextValues}>
+            { children }
+        </PokemonContext.Provider>
     )
 }
 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,9 +3,12 @@ import './index.css';
 import App from './App';
 
 // TODO: Import the PokemonProvider and wrap it around the App component
+import PokemonProvider from './context/PokemonProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App />
+    <PokemonProvider>
+        <App />
+    </PokemonProvider>
 );
