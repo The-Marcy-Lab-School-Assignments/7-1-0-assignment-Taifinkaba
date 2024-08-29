09f7260f6d0faf16360c6a8c86cc77e260ebe0f7
Taifinkaba <taifinkabad@gmail.com>
2024-08-29 11:25:28 -0400

    done

--- vite-project/src/components/PokemonForm.jsx
+++ vite-project/src/components/PokemonForm.jsx
@@ -1,11 +1,10 @@
 import React, {useState, useContext} from "react"
 import PokemonContext from "../context/PokemonContext"
-import handleFetch from "../utils/handleFetch"
 
 const PokemonForm = () => {
     const {allPokemon, setAllPokemon} = useContext(PokemonContext)
 
-    const {formData, setFormData} = useState({
+    const [formData, setFormData] = useState({
         name: '',
         hp: '',
         font:'',
@@ -15,10 +14,10 @@ const PokemonForm = () => {
     const handleChange = (e) =>{
         const {name, value} = e.target
 
-        setFormData((prevData)=({
-            ...prevData,
+        setFormData({
+            ...formData,
             [name]:value
-        }))
+        })
     }
 
     const handleSubmit = async (event) => {
@@ -45,23 +44,23 @@ const PokemonForm = () => {
     return (
         <div>
             <h3>Add a Pokemon!</h3>
-            <form className="ui form">
+            <form className="ui form" onSubmit={handleSubmit}>
                 <div className="four fields" widths="equal">
                     <div className="field ui fluid">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" />
+                        <input type="text" name="name" placeholder="Name" value={formData['name']} onChange={handleChange} />
                     </div>
                     <div className="field ui fluid">
                         <label>HP</label>
-                        <input type="text" name="hp" placeholder="HP" />
+                        <input type="text" name="hp" placeholder="HP" value={formData['hp']} onChange={handleChange}/>
                     </div>
                     <div className="field ui fluid">
                         <label>Front Image URL</label>
-                        <input type="text" name="front" placeholder="url" />
+                        <input type="text" name="front" placeholder="url" value={formData['front']} onChange={handleChange}/>
                     </div>
                     <div className="field ui fluid">
                         <label>Back Image URL</label>
-                        <input type="text" name="back" placeholder="url" />
+                        <input type="text" name="back" placeholder="url" value={formData['back']} onChange={handleChange}/>
                     </div>
                 </div>
                 <button className="ui button" type="submit">Submit</button>
